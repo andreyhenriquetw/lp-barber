0f4154c4f76bd06e698eb0fe770033ea495448b9
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-11-26 18:42:29 -0300

    feat: add Header and component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
+import Image from "next/image"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 
 const Home = () => {
   return (
     <div>
-      <Button>test</Button>
+      {/* HEADER */}
+      <Header />
+
+      {/* Banner logo */}
+      <div className="relative mt-1 h-[500px] w-full">
+        <Image
+          alt="Agende com LP Barber"
+          src="/logolpofc.jpg"
+          fill
+          className="rounded-xl object-cover"
+        />
+        <Button
+          className="btn-entrando shine-button"
+          style={{
+            position: "absolute",
+            right: "14px",
+            bottom: "33px",
+            padding: "12px 20px",
+            borderRadius: "12px",
+            color: "#FFD700",
+            fontSize: "14px",
+            fontWeight: 600,
+            boxShadow: "0px 4px 10px rgba(0,0,0,0.25)",
+            border: "1.5px solid #FFD700",
+            cursor: "pointer",
+            transition: "all 0.2s ease",
+            background: "#000000",
+            overflow: "hidden",
+          }}
+        >
+          AGENDAR AGORA
+        </Button>
+      </div>
     </div>
   )
 }
