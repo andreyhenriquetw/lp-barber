0d26667157d26efd88fbdde0048b641027188684
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-11-26 21:35:14 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -16,6 +19,8 @@ const Home = () => {
           fill
           className="rounded-xl object-cover"
         />
+
+        {/* BOTAO AGENDAR */}
         <Button
           className="btn-entrando shine-button"
           style={{
@@ -37,6 +42,34 @@ const Home = () => {
         >
           AGENDAR AGORA
         </Button>
+      </div>
+
+      {/* DIV P-5 */}
+      <div className="p-5">
+        {/* AGENDAMENTO */}
+
+        <Card>
+          <CardContent className="-m-2 flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 pl-6">
+              <Badge>Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://cultura.uol.com.br/webstories/2023/05/como-garantir-uma-barba-bem-feita/assets/5.jpeg" />
+                </Avatar>
+                <p className="text-sm">LP Barbearia</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-6">
+              <p className="text-sm">Novembro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">12:12</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
