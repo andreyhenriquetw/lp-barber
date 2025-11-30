d7a0ffea3e063387f41ed42856dbf446d0f52c6b
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-11-30 00:00:28 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -4,8 +4,16 @@ import { Button } from "./ui/button"
 import { CalendarIcon, HomeIcon, LogInIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -14,7 +22,34 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </div>
 
-      <div className="flex items-center gap-3 border-b border-solid px-5 pt-0 pb-4">
+      <div className="flex items-center justify-between gap-3 border-b border-solid px-5 pt-0 pb-4">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                alt="Fazer login com o Google"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*
         <Avatar>
           <AvatarImage src="https://img.freepik.com/fotos-premium/homem-das-cavernas-ou-retrato-3d-realista-neandertal_691560-1082.jpg" />
         </Avatar>
@@ -23,6 +58,7 @@ const SidebarSheet = () => {
           <p className="font-bold">Andrey Henrique</p>
           <p className="text-xs">Andreyhenriquetw3@gmail.com</p>
         </div>
+       */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid px-5 pt-0 pb-4">
