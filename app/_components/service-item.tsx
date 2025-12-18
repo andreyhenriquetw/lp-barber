ddf7540d4f1b6e2628cd29ea78da6ae39d1ab5c7
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-12-18 10:06:43 -0300

    feat: add correção do serviceitem

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -207,7 +207,7 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
 
                     <div className="mt-1 border-b pb-3">
                       <Calendar
-                        className="w-full overflow-hidden"
+                        className="w-full origin-top scale-[1.05] overflow-hidden"
                         mode="single"
                         locale={ptBR}
                         buttonVariant="outline"
